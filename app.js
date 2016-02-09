a3d62c759d1c56d0572924e4346f2bb0bfc85bf0
jwabbitt <jrabbitt1110@gmail.com>
2016-02-09 09:37:53 -0700

    Completed Part 2 of Assignment 1

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 var reps = require('./lib/reps');
+
+var express = require('express');
+var app = express();
+
+app.get('/all/by-zip/:zip', function(req, res, next) {
+  reps.allByZip(req.params.zip, function(err, people) {
+    if (err) { return next(err);}
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-name/:name', function(req, res, next) {
+  reps.repsByName(req.params.name, function(err, people) {
+    if (err) {return next(err);}
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-state/:state', function(req, res, next) {
+  reps.repsByState(req.params.state, function(err, people) {
+    if (err) {return next(err);}
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-name/:name', function(req, res, next) {
+  reps.sensByName(req.params.name, function(err, people) {
+    if (err) {return next(err);}
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-state/:state', function(req, res, next) {
+  reps.sensByState(req.params.state, function(err, people) {
+    if (err) {return next(err);}
+    res.json(people);
+  });
+});
+
+
+module.exports = app;
