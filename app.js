86a749c97e0f1e7452f183c5baf6b7339cb6eec5
jwabbitt <jrabbitt1110@gmail.com>
2016-02-17 13:02:31 -0700

    Finished getting the basics working for Part 3

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
+'use strict';
+
 var reps = require('./lib/reps');
 
 var express = require('express');
 var app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res, next) {
+  var method;
+  if (req.query.type === 'zip') {
+    method = reps.allByZip;
+  } else if (req.query.type === 'name') {
+    method = reps.repsByName;
+  } else if (req.query.type === 'state') {
+    method = reps.repsByState;
+  }
+
+  if (method) {
+    method(req.query.search, function(err, people) {
+      if (err) {return next(err); }
+      res.render('index', {
+        reps: people,
+        type: req.query.type,
+        search: req.query.search,
+      });
+    });
+  } else {
+  res.render('index');
+  }
+});
 
 app.get('/all/by-zip/:zip', function(req, res, next) {
   reps.allByZip(req.params.zip, function(err, people) {
@@ -38,5 +69,8 @@ app.get('/sens/by-state/:state', function(req, res, next) {
   });
 });
 
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
+});
 
 module.exports = app;
